0295edeb4ea9457eddd4f6d626ed12b5cefd7139
Tóth Kristóf <tothkrisi@gmail.com>
2019-03-13 09:36:32 +0100

    add code view source code

---
+++ codes/codeview.java
@@ -0,0 +1,19 @@
+codeView = view.findViewById(R.id.codeView);
+codeView.setTheme(CodeViewTheme.RAILSCASTS).fillColor();
+codeView.getSettings().setBuiltInZoomControls(true);
+codeView.getSettings().setDisplayZoomControls(false);
+codeView.getSettings().setTextZoom(50);
+codeView.setWebViewClient(new WebViewClient() {
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+        Log.d(Constants.TAG, "onPageStarted: loading page");
+    }
+    
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        Log.d(Constants.TAG, "onPageFinished: page loaded");
+        loading.setVisibility(View.GONE);
+    }
+});
