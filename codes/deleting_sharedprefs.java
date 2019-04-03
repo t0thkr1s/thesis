b76e1c49b5dcc1c2bf29c731a21996f26151390b
Tóth Kristóf <tothkrisi@gmail.com>
2019-04-03 11:41:11 +0200

    update code

--- codes/deleting_sharedprefs.java
+++ codes/deleting_sharedprefs.java
@@ -1,6 +1,5 @@
  public static void deleteCredentials(@NonNull Context context) {
-    SharedPreferences.Editor editor = context.getSharedPreferences(
-        context.getPackageName(), Context.MODE_PRIVATE).edit();
+    SharedPreferences.Editor editor = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE).edit();
     editor.clear();
     editor.apply();
 }
--- codes/saving_sharedpref.java
+++ codes/saving_sharedpref.java
@@ -1,6 +1,5 @@
 public static void saveCredentials(@NonNull Context context, String username, String password) {
-    SharedPreferences.Editor editor = context.getSharedPreferences(
-        context.getPackageName(), Context.MODE_PRIVATE).edit();
+    SharedPreferences.Editor editor = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE).edit();
     editor.putString(Constants.USERNAME_KEY, username);
     editor.putString(Constants.PASSWORD_KEY, password);
     editor.apply();
