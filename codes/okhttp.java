1c7323a3d311742dc0ee12d8f7cebb9b8b19fca2
Tóth Kristóf <tothkrisi@gmail.com>
2019-04-08 15:20:19 +0200

    add okhttp code

---
+++ codes/okhttp.java
@@ -0,0 +1,35 @@
+OkHttpClient okHttpClient = new OkHttpClient();
+
+RequestBody formBody = new MultipartBody.Builder()
+        .setType(MultipartBody.FORM)
+        .addFormDataPart("apk", file.getName(), RequestBody.create(MediaType.parse(Constants.APK_MEDIA_TYPE), file))
+        .build();
+
+ HttpUrl httpUrl = new HttpUrl.Builder()
+        .scheme(Constants.PROTOCOL) // protocol scheme
+        .host(Constants.HOST) // host name or IP
+        .port(Constants.PORT) // port number
+        .addPathSegment("upload") // path segment
+        .build();
+
+final Request request = new Request.Builder()
+        .url(httpUrl)
+        .header("Authorization", "Basic " + // authorization header
+            SharedPrefUtil.getCredentials(getApplicationContext()))
+        .post(formBody)
+        .build();
+
+okHttpClient.newCall(request).enqueue(new Callback() {
+    @Override
+    public void onFailure(final Call c, final IOException e) {
+        Log.e(Constants.TAG, "onFailure: " + e.getMessage(), e);
+        // ... snip ...
+    }
+
+    @Override
+    public void onResponse(Call c, final Response response) {
+        Log.d(Constants.TAG, "onResponse: " + response.headers());
+        // send local broadcast
+        // ... snip ...
+    }
+});
